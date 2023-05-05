8294c2082c25dec4cf5e1850c12d0d635643326c
RichaRokadiya <richarajeshrokadiya@gmail.com>
2023-05-05 16:17:30 +0530

    Using async await

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,42 +4,78 @@ const router = express.Router();
 require('../db/mongoose');
 const User = require('../model/userSchema');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.send('Hello from user router');
 });
 
-router.post('/token', (req, res) => {
-    // console.log(req.body);
 
-    // THIS
-    // if (!req.body.token) {
-    //     return res.status(422).json({ error: "Please enter the token id!" });
-    // }
 
-    // OR
-    const { token } = req.body;     // to add multiple fields, ex { name, email, password } = req.body;
-    if(!token) {                    // (!name || !email || !password) {}
-        return res.status(422).json({ error: "Please enter the token id!" });
-    }
+// ####### Using Promises ######
+// router.post('/token', (req, res) => {
+//     // console.log(req.body);
+
+//     // THIS
+//     // if (!req.body.token) {
+//     //     return res.status(422).json({ error: "Please enter the token id!" });
+//     // }
+
+//     // OR
+//     const { token } = req.body;     // to add multiple fields, ex { name, email, password } = req.body;
+//     if(!token) {                    // (!name || !email || !password) {}
+//         return res.status(422).json({ error: "Please enter the token id!" });
+//     }
 
 
     
-    User.findOne({ token: token })
-    .then((tokenExist) => {
+//     User.findOne({ token: token })
+//     .then((tokenExist) => {
 
-        // NOT REQD
+//         // NOT REQD
+//         // if(tokenExist) {
+//         //     return res.status(422).json({ error: "Token already exists"});
+//         // }
+
+//         const user = new User({ token });
+
+//         user.save().then(() => {
+//             res.status(201).json({ message: "Token entered successfully!"});
+//         }).catch((err) => {
+//             res.status(500).json({ error: "Failed to add token!"});
+//         })
+//     }).catch(err => { console.log(err); });
+// });
+// ###### End ######
+
+
+// ###### Using async await ######
+router.post('/token', async (req, res) => {
+    const { token } = req.body;
+    if(!token) {
+        res.status(422).json({ error: "Please enter the token id"});
+    }
+
+
+    try {
+        // Whenever u see promise, it means u have to wait
+        const tokenExist = await User.findOne({ token: token });
         // if(tokenExist) {
         //     return res.status(422).json({ error: "Token already exists"});
         // }
 
         const user = new User({ token });
 
-        user.save().then(() => {
-            res.status(201).json({ message: "Token entered successfully!"});
-        }).catch((err) => {
+        const userToken = await user.save();
+
+        if (userToken) {
+            res.status(201).json({ message: "Token entered successfully!" });
+        } else {
             res.status(500).json({ error: "Failed to add token!"});
-        })
-    }).catch(err => { console.log(err); });
+        }
+        
+    } catch(err) {
+        console.log(err);
+    }
 });
+
 
 module.exports = router;
