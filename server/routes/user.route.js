32f9a18167e08c2239ea9f989658fd3e94173f1d
RichaRokadiya <richarajeshrokadiya@gmail.com>
2023-05-05 15:39:43 +0530

    storing token to DB

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/mongoose');
+const User = require('../model/userSchema');
+
 router.get('/', (req, res) => {
     res.send('Hello from user router');
 });
 
 router.post('/token', (req, res) => {
-    console.log(req.body);
-    res.json({message: req.body});
-    // res.send('My token id page');
+    // console.log(req.body);
+
+    // THIS
+    // if (!req.body.token) {
+    //     return res.status(422).json({ error: "Please enter the token id!" });
+    // }
+
+    // OR
+    const { token } = req.body;     // to add multiple fields, ex { name, email, password } = req.body;
+    if(!token) {                    // (!name || !email || !password) {}
+        return res.status(422).json({ error: "Please enter the token id!" });
+    }
+
+
+    
+    User.findOne({ token: token })
+    .then((tokenExist) => {
+
+        // NOT REQD
+        // if(tokenExist) {
+        //     return res.status(422).json({ error: "Token already exists"});
+        // }
+
+        const user = new User({ token });
+
+        user.save().then(() => {
+            res.status(201).json({ message: "Token entered successfully!"});
+        }).catch((err) => {
+            res.status(500).json({ error: "Failed to add token!"});
+        })
+    }).catch(err => { console.log(err); });
 });
 
 module.exports = router;
